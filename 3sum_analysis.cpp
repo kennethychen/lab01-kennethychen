9f331bc6a1e3567056c63884a27f7a6cf7d7a9ca
kennethychen <kennethychen@ucsb.edu>
2025-04-11 00:35:05 -0700

    Update 3sum_analysis.cpp

--- 3sum_analysis.cpp
+++ 3sum_analysis.cpp
@@ -1,6 +1,9 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <chrono>
+#include <cstdlib>
+#include <random>
 using namespace std;
 
 class Solution {
@@ -35,37 +38,27 @@ public:
 
 int main() {
     srand(time(0));
-    
     vector<int> sizes = {100, 200, 400, 800, 1600, 3200, 6400};
-
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> big(-100000, 100000);
     uniform_int_distribution<> small(1, 1000);
-
     for (int n : sizes) {
         vector<int> nums;
-
         for (int i = 0; i < n - 3; ++i) {
             nums.push_back(big(gen));
         }
-
         int a = small(gen);
         int b = small(gen);
         nums.push_back(-a);
         nums.push_back(-b);
         nums.push_back(a + b);
-
         shuffle(nums.begin(), nums.end(), gen);
-
         auto start = chrono::high_resolution_clock::now();
         vector<vector<int>> result = threeSum(nums);
         auto end = chrono::high_resolution_clock::now();
-
         double time_ms = chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0;
-
         cout << "n = " << n << " - time = " << time_ms << " ms, triplets: " << result.size() << endl;
     }
-
     return 0;
 }
